dfb32a015e956db97bc40afcac5c2ce4daac1e26
sergio15dev <sergio15.dev@gmail.com>
2023-09-29 20:26:10 -0500

    new files

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-template<typename T>
-void BubbleSort(std::vector<T>& V){
-    
+
+void BubbleSort(std::vector<int>& A){
+    int n = A.size();
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                std::swap(A[j], A[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }    
 }
 
-template<typename T>
-void PrintVector(std::vector<T>& V){
-    for(int i=0; i<V.size(); i++)
-        std::cout<<V[i]<<" ";
+void PrintVector(const std::vector<int>& A){
+    for(int i=0; i<A.size(); i++)
+        std::cout<<A[i]<<" ";
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& A) {
+    PrintVector(A);
+    return os;
 }
 
 int main(){
-    std::vector<int> vector1 = {5, 1, 4, 11, 5, 9, 2, 6, 15, 3, 7};
-    PrintVector(vector1);
+    std::vector<int> vector1 = {5, 1, 4, 11, 18, 9, 13, 15, 3, 7};
+    std::cout<<vector1<<"\n";
+    BubbleSort(vector1);
+    std::cout<<vector1<<"\n";
     return 0;
 }
