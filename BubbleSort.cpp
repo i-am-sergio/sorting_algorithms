45ff2802d85f929dd100439771fec30e0fafd075
sergio15dev <sergio15.dev@gmail.com>
2023-09-29 20:35:20 -0500

    new files

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 
@@ -19,20 +20,38 @@ void BubbleSort(std::vector<int>& A){
     }    
 }
 
-void PrintVector(const std::vector<int>& A){
+void PrintVector(const std::vector<int>& A, std::ostream& os = std::cout){
     for(int i=0; i<A.size(); i++)
-        std::cout<<A[i]<<" ";
+        os << A[i] <<" ";
 }
 
 std::ostream& operator<<(std::ostream& os, const std::vector<int>& A) {
-    PrintVector(A);
+    PrintVector(A, os);
     return os;
+}
+
+void ShowInTerminal(std::vector<int>& vector){
+    std::cout<<vector<<"\n";
+    BubbleSort(vector);
+    std::cout<<vector<<"\n";
+}
+void ShowInFile(std::vector<int>& vector){
+    std::ofstream archivo("output.txt");
+    if (archivo.is_open()) {
+        archivo << vector << "\n\n";
+        BubbleSort(vector);
+        archivo << vector;
+        archivo.close();
+        std::cout << "La salida se ha guardado en 'output.txt'.\n";
+    } else {
+        std::cerr << "No se pudo abrir el archivo para escribir.\n";
+    }
+
 }
 
 int main(){
     std::vector<int> vector1 = {5, 1, 4, 11, 18, 9, 13, 15, 3, 7};
-    std::cout<<vector1<<"\n";
-    BubbleSort(vector1);
-    std::cout<<vector1<<"\n";
+    ShowInTerminal(vector1);
+    // ShowInFile(vector1);
     return 0;
 }
