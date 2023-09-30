6960d54e6405ca1673068d1035a17128105534f7
sergio15dev <sergio15.dev@gmail.com>
2023-09-29 20:48:04 -0500

    Insertion Sort Complete

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,44 +2,42 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-
+#include "Utilidades.h"
 
 void InsertionSort(std::vector<int>& A){
-
-}
-
-void PrintVector(const std::vector<int>& A, std::ostream& os = std::cout){
-    for(int i=0; i<A.size(); i++)
-        os << A[i] <<" ";
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<int>& A) {
-    PrintVector(A, os);
-    return os;
+    int i, j, current, size = A.size();
+    for ( i=1; i<size; i++){
+        current = A[i];
+        j = i - 1;
+        while(j>=0 && A[j]>current){
+            A[j+1] = A[j];
+            j -= 1;
+        }
+        A[j+1] = current;
+    }
 }
 
 void ShowInTerminal(std::vector<int>& vector){
     std::cout<<vector<<"\n";
-    BubbleSort(vector);
+    InsertionSort(vector);
     std::cout<<vector<<"\n";
 }
 void ShowInFile(std::vector<int>& vector){
     std::ofstream archivo("output.txt");
     if (archivo.is_open()) {
         archivo << vector << "\n\n";
-        BubbleSort(vector);
+        InsertionSort(vector);
         archivo << vector;
         archivo.close();
-        std::cout << "La salida se ha guardado en 'output.txt'.\n";
+        std::cout << "Se ha guardado en 'output.txt'...\n";
     } else {
-        std::cerr << "No se pudo abrir el archivo para escribir.\n";
+        std::cerr << "No se pudo abrir el archivo para escribir\n";
     }
-
 }
 
 int main(){
-    std::vector<int> vector1 = {5, 1, 4, 11, 18, 9, 13, 15, 3, 7};
-    ShowInTerminal(vector1);
-    // ShowInFile(vector1);
+    std::vector<int> vector1 = {5, 1, 4, 11, 18, 9,100, 13, 15, 3, 7};
+    // ShowInTerminal(vector1);
+    ShowInFile(vector1);
     return 0;
 }
